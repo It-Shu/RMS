107f00410aabed76d9b64427647f99683cbaed55
It-Shu <it.shustov@gmail.com>
2021-10-03 14:45:24 +0300

    refactor css for Enter.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Enter from "./components/enter/Enter";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Registration from "./components/registration/Registration";
 import OrganizationData from "./components/organizationData/OrganizationData";
 
 function App() {
 
     return (
-        <BrowserRouter>
+            <div>
                 <Route exact path={'/'} component={Enter}/>
                 <Route path={'/registration'} component={Registration}/>
                 <Route path={'/data'} component={OrganizationData}/>
-        </BrowserRouter>
+            </div>
     );
 }
 
--- src/components/enter/Enter.tsx
+++ src/components/enter/Enter.tsx
@@ -10,50 +10,61 @@ type EnterPropsType = {}
 const Enter = (props: EnterPropsType) => {
     return (
         //
-        <div className={styles.body}>
-            <form className={styles.form}>
-                <h1 className={styles.title}>Вход</h1>
-                <div className={styles.inputForm}>
-                    <div className={styles.inputTitle}><label>Email</label></div>
-                    <div ><input className={styles.input} type="email"/></div>
-                </div>
-                <div className={styles.passwordHref}>
-                    <a href={''}>Забыли пароль?</a>
-                </div>
-                <div className={styles.inputForm}>
-                    <div className={styles.inputTitle}><label htmlFor="">Пароль</label></div>
-                    <div ><input className={styles.input} type="password"/></div>
-                </div>
-                <div className={styles.buttonForm}>
-                    <div >
+        // <div className={styles.body}>
+        <div>
+            <section className={styles.body}>
+                <form className={styles.form}>
+                    <h1 className={styles.title}>Вход</h1>
+                    <div className={styles.inputForm}>
+                        <div className={styles.inputTitle}>
+                            <label>Email</label>
+                        </div>
+                        <div className={styles.inputDiv}>
+                            <input className={styles.input} type="email"/>
+                        </div>
+                    </div>
+                    <div className={styles.passwordHref}>
+                        <a className={styles.forgotPass} href={''}>Забыли пароль?</a>
+                    </div>
+                    <div className={styles.inputForm}>
+
+                        <div className={styles.inputTitle}>
+                            <label>Пароль</label>
+                        </div>
+                        <div className={styles.inputDiv}>
+                            <input className={styles.input} type="password"/>
+                        </div>
+
+                    </div>
+                    <div>
                         <button className={styles.button} type='submit' disabled={true}>Войти</button>
                     </div>
-                    <div >
+                    <div>
                         <NavLink to={'/registration'}>
                             <button className={styles.button}>Зарегистрироваться</button>
                         </NavLink>
                     </div>
-                </div>
-                <p>Или войдите с помощью других сервисов</p>
-                <div className={styles.otherEnters}>
-                    <div className={styles.icons}>
-                        <a href='https://www.google.com/'><img className={styles.googleIcon}
-                                                               src={googleIcon}
-                                                               alt="google"/></a>
+                    <p className={styles.titleOtherEnters}>Или войдите с помощью других сервисов</p>
+                    <div className={styles.otherEnters}>
+                        <div className={styles.icons}>
+                            <a href='https://www.google.com/'><img className={styles.googleIcon}
+                                                                   src={googleIcon}
+                                                                   alt="google"/></a>
+                        </div>
+                        <div className={styles.icons}>
+                            <a href='https://www.facebook.com/'><img className={styles.googleIcon}
+                                                                     src={facebookIcon}
+                                                                     alt="facebook"/></a></div>
+                        <div className={styles.icons}>
+                            <a href='https://www.apple.com/'><img className={styles.googleIcon}
+                                                                  src={appleIcon}
+                                                                  alt="apple"/></a>
+                        </div>
                     </div>
-                    <div className={styles.icons}>
-                        <a href='https://www.facebook.com/'><img className={styles.googleIcon}
-                                                                 src={facebookIcon}
-                                                                 alt="facebook"/></a></div>
-                    <div className={styles.icons}>
-                        <a href='https://www.apple.com/'><img className={styles.googleIcon}
-                                                              src={appleIcon}
-                                                              alt="apple"/></a>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </section>
             <footer className={styles.footer}>
-                <span>2021 OOO"РМ Солюшн"</span>
+                <span className={styles.span}>2021 OOO"РМ Солюшн"</span>
                 <nav className={styles.nav}>
                     <div className={styles.navFooter}><a href="">Правила пользования</a></div>
                     <div className={styles.navFooter}><a href="">Политика конфиденциальности</a></div>
